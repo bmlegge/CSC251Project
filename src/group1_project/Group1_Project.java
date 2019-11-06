f265c14fda41db4e8e899602e75cfc6607aa7045
leggeb9867 <leggeb9867@ATC15610.ad.faytechcc.edu>
2019-11-06 10:28:24 -0500

    HelloWorld

--- src/group1_project/Group1_Project.java
+++ src/group1_project/Group1_Project.java
@@ -11,9 +11,5 @@ public class Group1_Project
     public static void main(String[] args) 
     {
         System.out.println("Hello World.");
-        System.out.println("Hello again.");
-        System.out.println("Hello final.");
-        System.out.println("Hello again.");
-    }
     
 }
