5d9578043171a79cca086ed0a3bdf67c5f945114
WinStoker <31968363+WinStoker@users.noreply.github.com>
2019-11-20 15:49:12 -0500

    Menu Changes

--- src/groupproject/Main.java
+++ src/groupproject/Main.java
@@ -4,11 +4,46 @@
 package groupproject;
 
 import java.util.Scanner;
+import java.util.*;
 
 public class Main 
 {
       public static void main(String[] args)
       {
-          Menu.DisplayMenu();
+          String input = "";
+          boolean exit = false;
+          int userMenuSelection = 0;
+          Scanner keyboard = new Scanner(System.in);
+               
+          do
+          {
+            userMenuSelection = Menu.DisplayMenu(userMenuSelection, keyboard, input);
+         
+            try
+            {
+                //Decision Structure
+                switch(userMenuSelection)
+                {
+                    case 1:
+                        Brick.BrickMenu();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        System.exit(0);
+                        break;      
+                    default: 
+                        System.out.println(" Error ");
+                        break;
+                }            
+            }               
+            catch(InputMismatchException e)
+            {
+            }    
+          }while(exit == false);      
       }
 }
--- src/groupproject/Menu.java
+++ src/groupproject/Menu.java
@@ -3,57 +3,34 @@ Menu for user to select options from.
 */
 package groupproject;
 
+import java.util.InputMismatchException;
 import java.util.Scanner; 
-import java.util.*;
 
 public class Menu 
 {
-    public static void DisplayMenu()
+    public static int DisplayMenu(int userMenuSelection, Scanner keyboard,
+            String input)
     {
-        boolean exit = false;
-        int userMenuSelection = 0;
-        Scanner keyboard = new Scanner(System.in);
-        
-        do
+        try
         {
-            try
-            {
-                //Decision Structure
-                switch(userMenuSelection)
-                {
-                    case 1:
-                        Brick.BrickMenu();
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        System.exit(0);
-                        break;      
-                    default:
-                        System.out.println("errora");
-                }
-                System.out.println(
-                        "                      Menu                        "
-                       +"\n------------------------------------------------"
-                       +"\n1. Brick Selection"// 
-                       +"\n2."//
-                       +"\n3."//
-                       +"\n4."// 
-                       +"\n5. Exit "//Exit program
-                       +"\n------------------------------------------------"
-                       +"\n\nEnter a number from the menu: "); 
-                System.out.print(">> ");
-                userMenuSelection = keyboard.nextInt(); 
-            }
-            catch(InputMismatchException e)
-            {
-                System.out.println(e);
-                break;
-            }
-        }while(exit == false);
+            System.out.println(
+                    "                      Menu                        "
+                   +"\n------------------------------------------------"
+                   +"\n1. Brick Selection"// 
+                   +"\n2."//
+                   +"\n3."//
+                   +"\n4."// 
+                   +"\n5. Exit "//Exit program
+                   +"\n------------------------------------------------"
+                   +"\n\nEnter a number from the menu: "); 
+            System.out.print(">> "); 
+            input = keyboard.nextLine();
+            userMenuSelection = Integer.parseInt(input); //Convert str to int  
+  
+        }
+        catch(NumberFormatException e)
+        {                         
+        }       
+        return userMenuSelection;
     }
 }
