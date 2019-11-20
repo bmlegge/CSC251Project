8c8858b355802818c07c192b333359ff50517ff2
bmlegge <36506641+bmlegge@users.noreply.github.com>
2019-11-20 10:38:29 -0500

    changes

--- src/groupproject/Brick.java
+++ src/groupproject/Brick.java
@@ -11,19 +11,26 @@ import java.util.ArrayList;
 
 public class Brick 
 {
+    private String _name;
     private double _length;
     private double _height;
     private double _width;
     private String _color;
     private double _cost;
     
-    public Brick(double l, double h, double w, String color, double cost)
+    public Brick(String name, double l, double h, double w, String color, double cost)
     {
+        _name = name;
         _length = l;
         _height = h;
         _width = w;
         _color = color;
         _cost = cost;
+    }
+    
+    public void setName(String name)
+    {
+        _name = name;
     }
     
     public void setLength(double l)
@@ -49,6 +56,11 @@ public class Brick
     public void setCost(double cost)
     {
         _cost = cost;
+    }
+    
+    public String getName()
+    {
+        return _name;
     }
     
     public double getLength()
@@ -91,17 +103,19 @@ public class Brick
                 String[] values = line.split(",");
 
                 bricks.add(new Brick
-                         (Double.parseDouble(values[0]), 
+                         (values[0],
                           Double.parseDouble(values[1]), 
                           Double.parseDouble(values[2]), 
-                          values[3], 
-                          Double.parseDouble(values[4])));
+                          Double.parseDouble(values[3]), 
+                          values[4], 
+                          Double.parseDouble(values[5])));
 
-                System.out.println("Brick length is " + values[0] + 
-                        " Brick height is " + values[1] +
-                        " Brick width is " + values[2] +
-                        " Brick color is " + values[3] +
-                        " Brick costs is $" + values[4] + ".");
+                System.out.println("Brick name is " + values[0] +
+                        " Brick length is " + values[1] + 
+                        " Brick height is " + values[2] +
+                        " Brick width is " + values[3] +
+                        " Brick color is " + values[4] +
+                        " Brick costs is $" + values[5] + ".");
             }
             inputFile.close();
         }
