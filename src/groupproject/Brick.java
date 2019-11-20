267601df8201f36551e09b23874c81cf900a28fc
WinStoker <31968363+WinStoker@users.noreply.github.com>
2019-11-20 12:54:56 -0500

    Created Menu

---
+++ src/groupproject/Brick.java
@@ -0,0 +1,14 @@
+/*
+ * Brick Class
+ */
+package groupproject;
+import java.util.Scanner;
+
+public class Brick 
+{
+    public static void BrickMenu()
+    {
+        System.out.println("");
+        System.out.println("This is a test !");
+    }
+}
--- src/groupproject/Menu.java
+++ src/groupproject/Menu.java
@@ -4,34 +4,56 @@ Menu for user to select options from.
 package groupproject;
 
 import java.util.Scanner; 
+import java.util.*;
 
 public class Menu 
 {
     public static void DisplayMenu()
     {
-       boolean exit = false; // Keeps the loop going    
-       Scanner keyboard = new Scanner(System.in);//Object for keyboard input
-       
-       try
-       {
-           do
-           {
-               System.out.println("Please make a selection from the menu"
-                                + " below: ");
-               
-               System.out.println("-----MENU-----"
-                       + "1. Choose brick type: "
-                       + "2. Choose ");
-               
-               break;//Prevents an infinite loop
-               
-           }while(exit == false);
-           
-       }
-       catch(Exception ex)
-       {
-           System.out.println(ex); //print any errors;(for testing)
-       }
- 
+        boolean exit = false;
+        int userMenuSelection = 0;
+        Scanner keyboard = new Scanner(System.in);
+        
+        do
+        {
+            try
+            {
+                //Decision Structure
+                switch(userMenuSelection)
+                {
+                    case 1:
+                        Brick.BrickMenu();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        System.exit(0);
+                        break;      
+                    default:
+                        System.out.println("errora");
+                }
+                System.out.println(
+                        "                      Menu                        "
+                       +"\n------------------------------------------------"
+                       +"\n1. Brick Selection"// 
+                       +"\n2."//
+                       +"\n3."//
+                       +"\n4."// 
+                       +"\n5. Exit "//Exit program
+                       +"\n------------------------------------------------"
+                       +"\n\nEnter a number from the menu: "); 
+                System.out.print(">> ");
+                userMenuSelection = keyboard.nextInt(); 
+            }
+            catch(InputMismatchException e)
+            {
+                System.out.println(e);
+                break;
+            }
+        }while(exit == false);
     }
 }
