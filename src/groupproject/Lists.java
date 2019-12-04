cec2c0df400131dbe066bd54f693ffde960492d3
bmlegge <36506641+bmlegge@users.noreply.github.com>
2019-12-04 09:58:09 -0500

    Added new features

---
+++ src/groupproject/Lists.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package groupproject;
+
+import java.util.*;
+
+public class Lists 
+{
+    //Create List of Objects that can be used in the project.
+    public static List<Brick> newBricks = new ArrayList<>();
+    public static List<Concrete> newConcrete = new ArrayList<>();
+}
---
+++ src/groupproject/Wall.java
@@ -0,0 +1,17 @@
+/*
+ * 
+*/
+package groupproject;
+
+public class Wall 
+{
+    public static void BuildBrickPart(double l, double h, double w)
+    {
+        System.out.println("len: " +l+ " he: " +h+ " w: " +w);
+    }
+    
+    public static void BuildConcretePart(double pounds)
+    {
+        System.out.println("pounds: " +pounds);
+    }
+}
