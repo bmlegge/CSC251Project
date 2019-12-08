971286c476aaba3a10b5473567ba698d78d3b33e
mccartns9115 <mccartns9115@Chihuahua>
2019-12-07 21:32:03 -0500

    completed wall calculations

--- src/groupproject/Wall.java
+++ src/groupproject/Wall.java
@@ -9,9 +9,10 @@ public class Wall
 {
     public static Scanner keyboard = new Scanner(System.in);
     public static final double MORTAR=0.375;
-    
+    //change to build_planter_wall
     public static void BuildBrickWall(double l, double h, double w)
     {
+        
         double height = h+MORTAR;//4
         double length = l+MORTAR; //8.375
         double width= w+MORTAR;//2.625
@@ -23,29 +24,60 @@ public class Wall
 //        double wall_height=keyboard.nextDouble();
         
         double wall_length= 1200;
-        double wall_height= 60;
+        double wall_height= 24;
         
         //TODO need to convert wall_height/wall_length into inches (*12)
-        double design1= wall_length/width;           
-        double design2= 2*(wall_length/height);
+//        double design1= wall_length/width;           
+//        double design2= 2*(wall_length/height);
+//        
+//        double midSection= (2*height)+length;
+//        double remainingWall= wall_height - midSection;    //43.625    
+        double numOfRows= wall_height/height;       //17
+        double bricksAcrossRow= (wall_length/length)+1;   //8
+        double totalBricks= (numOfRows*bricksAcrossRow) * 2;       
         
-        double midSection= (2*height)+length;
-        double remainingWall= wall_height - midSection;    //43.625    
-        double numOfRows= remainingWall/height;       //17
-        double bricksAcrossRow= wall_length/length;   //8
-        double design3= numOfRows*bricksAcrossRow;       
+         
         
-        double totalBricks= design1+design2+design3;
+        System.out.println("Rows:" +numOfRows +
+                "\nBricks Across: " +bricksAcrossRow +
+                "\nTotal Bricks: " + totalBricks);
         
-        System.out.println("Long brick amount: " + design1 +
-                "\nDouble brick amount: " + design2 + 
-                "\nMiddle section: " + midSection + 
-                "\nRemaining wall: " + remainingWall + 
-                "\nNumber of rows: " + numOfRows +
-                "\nBricks across a row: " + bricksAcrossRow +
-                "\nRemaining brick amount: " + design3 +
-                "\nTotal bricks needed: " + totalBricks);
+        BuildBrickWall2( height,length, width);
+        
     }
+    
+    public static void BuildBrickWall2(double l, double h, double w)
+    {
+        double height = h+MORTAR;//4
+        double length = l+MORTAR; //8.375
+        double width= w+MORTAR;//2.625
+       
+//        System.out.println("Enter the length of the wall");
+//        double wall_length=keyboard.nextDouble();
+//        
+//        System.out.println("Enter the height of the wall");
+//        double wall_height=keyboard.nextDouble();
+        
+        double wall_length= 720;//60 feet
+        double wall_height= 84;//7 feet
+        double numOfRows= wall_height/height;       //17
+        double bricksAcrossRow= (wall_length/length)+1;   //8
+        double totalBricks= (numOfRows*bricksAcrossRow) * 2;       
+        
+         
+        
+        System.out.println("Rows:" +numOfRows +
+                "\nBricks Across: " +bricksAcrossRow +
+                "\nTotal Bricks: " + totalBricks);
+        
+        
+    }
+    
+    
+    
+    
+    
+    
     
     public static void BuildConcretePart(double pounds)
     {
